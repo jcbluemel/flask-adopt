e99601eece741eb68c5a741484993d8c13a71bb4
anela <anela89@gmail.com>
2022-06-17 15:27:51 -0700

    created EditPetForm

--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
 """Forms for adopt app."""
 
+from wsgiref.validate import validator
+from matplotlib.style import available
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, TextAreaField
+from wtforms import StringField, SelectField, TextAreaField, BooleanField
 from wtforms.validators import InputRequired, Optional, URL
 
 
@@ -10,22 +12,35 @@ class AddPetForm(FlaskForm):
 
     # TODO: add form validation
     name = StringField('Pet Name',
-        validators=[InputRequired()])
+                       validators=[InputRequired()])
     species = SelectField('Species',
-        choices=[
-            ('cat', 'Cat'),
-            ('dog', 'Dog'),
-            ('porcupine', 'Porcupine')],
-        validators=[InputRequired()])
+                          choices=[
+                              ('cat', 'Cat'),
+                              ('dog', 'Dog'),
+                              ('porcupine', 'Porcupine')],
+                          validators=[InputRequired()])
     photo_url = StringField('Photo URL',
-        validators=[Optional(), URL()])
+                            validators=[Optional(), URL()])
     age = SelectField('Age',
-        choices=[
-            ('baby', 'Baby'),
-            ('young', 'Young'),
-            ('adult', 'Adult'),
-            ('senior', 'Senior')],
-        # do we need this inputrequired validator for age?
-        validators=[InputRequired()])
+                      choices=[
+                          ('baby', 'Baby'),
+                          ('young', 'Young'),
+                          ('adult', 'Adult'),
+                          ('senior', 'Senior')],
+                      # do we need this inputrequired validator for age?
+                      validators=[InputRequired()])
     notes = TextAreaField('Additional notes',
-        validators=[Optional()])
+                          validators=[Optional()])
+
+
+class EditPetForm(FlaskForm):
+    """Form for editing pet."""
+
+    photo_url = StringField('Photo URL',
+                            validators=[Optional(), URL()])
+
+    notes = TextAreaField('Additional notes',
+                          validators=[Optional()])
+
+    available = BooleanField('Available', 
+                            validators=[Optional()])
