26e5ced1e2af9ed4895b365df76ed6ad1a55ca93
anela <anela89@gmail.com>
2022-06-17 12:28:11 -0700

    created form for adding pets

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 """Flask app for adopt app."""
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db
+from models import db, connect_db, Pet
+
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -22,3 +24,30 @@ db.create_all()
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+@app.get('/')
+def index():
+    """Display homepage with list of pets."""
+
+    pets = Pet.query.all()
+
+    return render_template('index.html', pets=pets)
+
+@app.route("/add", methods=["GET", "POST"])
+def pet_add():
+    """Pets add form; handle adding."""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        return redirect("/")
+
+    else:
+        return render_template(
+            "pet_add.html", form=form)
--- forms.py
+++ forms.py
@@ -1,1 +1,17 @@
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets."""
+
+    # TODO: add form validation
+    name = StringField('Pet Name')
+    species = StringField('Species')
+    photo_url = StringField('Photo URL')
+    age = SelectField('Age',
+                      choices=[('baby', 'Baby'), ('young', 'Young'),
+                               ('adult', 'Adult'), ('senior', 'Senior')])
+    notes = TextAreaField('Additional notes')
